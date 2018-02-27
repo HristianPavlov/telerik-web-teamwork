786a632e5e34d38962f1f84874e9f5c1458211fa
mstilyan <stilyan.mladenov@gmail.com>
2018-02-27 17:29:24 +0200

    rmv button disabling functionality added

--- MovieAficionado/src/addBtnEvent.js
+++ MovieAficionado/src/addBtnEvent.js
@@ -1,21 +1,26 @@
-$(document).on("click", ".add-btn", function(){
+$(document).on("click", ".add-btn", function () {
     var favourites = favouritesDatabase.favourites;;;;;;
     var id = $(this).attr('id').split("-")[0];
-    if(favourites[id]){
-        alert("Already in favourites!");
-    }
-    else{
-        var title = $(`#${id}-title`).html();
-        var year = $(`#${id}-year span`).html();
-        var genre = $(`#${id}-genre span`).html();
 
-        favouritesDatabase.addMovie(
-            {
-                imdbID: id,
-                Title: title,
-                Year: year,
-                Genre: genre
-            }
-        )
-    }
+    var buttonColorToRemove = "btn-outline-success";
+    var buttonColorToAdd = "btn-outline-danger";
+    var buttonMessage = "Added";
+
+    $(this).removeClass(buttonColorToRemove);
+    $(this).addClass(buttonColorToAdd);
+    $(this).text(buttonMessage);
+    $(this).attr("disabled", true);
+
+    var title = $(`#${id}-title`).html();
+    var year = $(`#${id}-year span`).html();
+    var genre = $(`#${id}-genre span`).html();
+
+    favouritesDatabase.addMovie(
+        {
+            imdbID: id,
+            Title: title,
+            Year: year,
+            Genre: genre
+        }
+    );
 })
