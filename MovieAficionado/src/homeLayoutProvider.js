b8f463a008a4415ca3d417062cf7e2faf6ae4e0d
Hristian <hristian.p.pavlov@abv.bg>
2018-03-01 09:58:57 +0200

    grammar errors you fucking monkeys

--- MovieAficionado/src/homeLayoutProvider.js
+++ MovieAficionado/src/homeLayoutProvider.js
@@ -3,7 +3,7 @@ var homeLayoutProvider = function(){
         html:  `<h1>MovieAficionado</h1>
                 <p>
                     MovieAficionado, also known as MA, is a website where you can search
-                    nformation about your favourite<br> Movies, TV Shows, Games and more.
+                    information about your favorite<br> Movies, TV Shows, Games and more.
                 </p>`
     }
 }
